87c55d24c3068a908590efda8a7382189872cad3
Ahmed Lekssays <lekssaysahmed@gmail.com>
2019-04-20 16:52:16 +0100

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: {registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "videos#index"
+  root to: "home#index"
 end
